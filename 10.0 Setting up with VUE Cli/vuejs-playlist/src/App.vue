ea61171afd6f64b7f12c9484f14a94e87d250b2a
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2020-10-08 18:12:56 +0545

    Added slots files

--- 10.0 Setting up with VUE Cli/vuejs-playlist/src/App.vue
+++ 10.0 Setting up with VUE Cli/vuejs-playlist/src/App.vue
@@ -7,22 +7,41 @@
     <h1>Hello Mister</h1>
 
     <app-footer v-bind:title="title" />
+    <app-slot>
+      <div slot="form-header">
+        <h3>A custom tite</h3>
+        <p>A very crucial information about form-header</p>
+      </div>
+
+      <div slot="form-fields">
+        <input type="text" placeholder="Enter name" required /><br /><br />
+        <input type="password" placeholder="Enter password" required /> <br />
+        <br />
+      </div>
+
+      <div slot="form-controls">
+        <button>Submit your form</button>
+      </div>
+    </app-slot>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SlotFile from "./components/SlotFile";
 
 export default {
   name: "App",
   components: {
     "app-header": Header,
     "app-footer": Footer,
+    "app-slot": SlotFile,
   },
   data: function () {
     return {
       title: "Independence day",
+      anotherTitle: "A super cool Dynamic title",
     };
   },
   methods: {
